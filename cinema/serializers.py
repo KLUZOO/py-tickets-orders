cba930ff77d1e86399ede82d3244089b8d9e2935
Sasha <kluzodota@gmail.com>
2025-07-20 13:05:20 +0300

    problem

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -138,9 +138,6 @@ class OrderCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         with transaction.atomic():
-            print("Hello")
-            print(validated_data)
-            print("Hello")
             tickets_data = validated_data.pop("tickets")
             order = Order.objects.create(**validated_data)
             for ticket_data in tickets_data:
